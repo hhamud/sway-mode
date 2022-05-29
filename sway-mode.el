733def8c1ff99572c58c2d091ca552dfe4560ab9
Hamza <hamza.h.hamud@gmail.com>
2022-05-29 16:45:25 +0100

    feat: add syntax highlighting for function call types

--- sway-mode.el
+++ sway-mode.el
@@ -5,13 +5,27 @@
   (require 'rx))
 
 
+(defvar function-call-highlights "\\(\\(?:\\w\\|\\s_\\)+\\)\\(<.+>\\)?\s*("
+  "Regex for general Sway function calls.")
+
+(defvar function-call-type-highlights "\\b\\([A-Za-z][A-Za-z0-9_]*\\|_[A-Za-z0-9_]+\\)\\(::\\)\\(<.*>\s*\\)\("
+  "Regex for Sway function calls with type.")
+
+(defvar top-level-declaration "\\b\\(library\\)\\s+\\([a-zA-Z_][a-zA-Z0-9_]*\\)"
+  "Regex for Sway top level declarations with name.")
+
+(defvar declarations-without-name '("contract" "script" "predicate")
+  "Sway specific declarations.")
+
+
 (setq sway-highlights
-      '(
-        ("abi" . font-lock-keyword-face)
-        ("contract" . font-lock-keyword-face)
-        ("library" . font-lock-keyword-face)
-        ("storage" . font-lock-keyword-face)
+      `(
+        (,(regexp-opt declarations-without-name 'word) . 'font-lock-keyword-face)
+        (,top-level-declaration . 'font-lock-keyword-face)
+        (,function-call-highlights . (1 'font-lock-function-name-face))
+        (,function-call-type-highlights . (1 'font-lock-function-name-face))
         ))
+
 
 ;;;###autoload
 (define-derived-mode sway-mode rust-mode
@@ -21,6 +35,6 @@
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.sw" . sway-mode))
 
-
+;; add to feature list
 (provide 'sway-mode)
 ;;; sway-mode.el ends here
