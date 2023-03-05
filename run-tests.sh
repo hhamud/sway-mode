993c2fcb3d8358ae19cd04cbf19cda960136c182
Federico Gimenez <federico.gimenez@gmail.com>
2023-03-05 09:54:35 +0100

    added CI checks

---
+++ run-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+EMACS="${EMACS:=emacs}"
+
+NEEDED_PACKAGES="cl-lib let-alist package-lint"
+
+INIT_PACKAGE_EL="(progn \
+  (require 'package) \
+  (push '(\"melpa\" . \"https://melpa.org/packages/\") package-archives) \
+  (package-initialize) \
+  (unless package-archive-contents \
+     (package-refresh-contents)) \
+  (dolist (pkg '(${NEEDED_PACKAGES})) \
+    (unless (package-installed-p pkg) \
+      (package-install pkg))))"
+
+# Refresh package archives, because the test suite needs to see at least
+# package-lint and cl-lib.
+"$EMACS" -Q -batch \
+         --eval "$INIT_PACKAGE_EL"
+
+"$EMACS" -Q -batch \
+         --eval "$INIT_PACKAGE_EL" \
+         -l package-lint.el \
+         -f package-lint-batch-and-exit \
+         sway-mode.el
