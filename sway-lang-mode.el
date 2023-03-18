92a6d42732a64b385bf29f972af12f4a1c03881d
Federico Gimenez <federico.gimenez@gmail.com>
2023-03-18 14:36:49 +0100

    renamed sway-mode -> sway-lang-mode

--- sway-lang-mode.el
+++ sway-lang-mode.el
@@ -1,4 +1,4 @@
-;;; sway-mode.el --- Major mode for sway  -*- lexical-binding: t; -*-
+;;; sway-lang-mode.el --- Major mode for sway  -*- lexical-binding: t; -*-
 
 ;; Version: 0.0.1
 ;; Author: Hamza Hamud
@@ -22,91 +22,90 @@
   (require 'lsp-mode)
   (require 'rust-mode))
 
-(defun sway-mode-fmt-custom (path)
+(defun sway-lang-mode-fmt-custom (path)
   "Formats sway files within the supplied PATH."
   (interactive "spath to toml file:")
   (shell-command "forc fmt --path %s" path))
 
-(defun sway-mode-fmt ()
+(defun sway-lang-mode-fmt ()
   "Formats a single sway file."
   (interactive)
   (let ((default-directory (expand-file-name "../")))
     (shell-command "forc fmt")))
 
-(defun sway-mode-test ()
+(defun sway-lang-mode-test ()
   "Run forc test."
   (interactive)
   (let ((default-directory (expand-file-name "../")))
     (shell-command "forc test")))
 
-(defun sway-mode-build ()
+(defun sway-lang-mode-build ()
   "Build a forc project."
   (interactive)
   (let ((default-directory (expand-file-name "../")))
     (shell-command "forc build")))
 
-(defun sway-mode-deploy ()
+(defun sway-lang-mode-deploy ()
   "Build a forc project."
   (interactive)
   (let ((default-directory (expand-file-name "../")))
     (shell-command "forc deploy")))
 
-(defvar sway-function-call-highlights "\\(\\(?:\\w\\|\\s_\\)+\\)\\(<.+>\\)?\s*("
+(defvar sway-lang-function-call-highlights "\\(\\(?:\\w\\|\\s_\\)+\\)\\(<.+>\\)?\s*("
   "Regex for general Sway function calls.")
 
-(defvar sway-function-call-type-highlights "\\b\\([A-Za-z][A-Za-z0-9_]*\\|_[A-Za-z0-9_]+\\)\\(::\\)\\(<.*>\s*\\)\("
+(defvar sway-lang-function-call-type-highlights "\\b\\([A-Za-z][A-Za-z0-9_]*\\|_[A-Za-z0-9_]+\\)\\(::\\)\\(<.*>\s*\\)\("
   "Regex for Sway function calls with type.")
 
-(defvar sway-declarations-without-name '("contract" "script" "predicate")
+(defvar sway-lang-declarations-without-name '("contract" "script" "predicate")
   "Sway specific declarations.")
 
-(defvar sway-type-level-declaration  "\\b\\(abi\\|library\\)\\s-"
+(defvar sway-lang-type-level-declaration  "\\b\\(abi\\|library\\)\\s-"
   "Sway specific type level declaration.")
 
-(defvar sway-highlights)
-(defvar sway-highlights
+(defvar sway-lang-highlights
       `(
-        (,(regexp-opt sway-declarations-without-name 'symbols) . 'font-lock-keyword-face)
-        (, sway-type-level-declaration . 'font-lock-keyword-face)
-        (,sway-function-call-highlights . (1 'font-lock-function-name-face))
-        (,sway-function-call-type-highlights . (1 'font-lock-function-name-face))))
+        (,(regexp-opt sway-lang-declarations-without-name 'symbols) . 'font-lock-keyword-face)
+        (, sway-lang-type-level-declaration . 'font-lock-keyword-face)
+        (,sway-lang-function-call-highlights . (1 'font-lock-function-name-face))
+        (,sway-lang-function-call-type-highlights . (1 'font-lock-function-name-face))))
 
 
 
 ;;; KeyMap
-(defvar sway-mode-map
+(defvar sway-lang-mode-map
   (let ((keymap (make-sparse-keymap)))
-    (define-key keymap (kbd "C-c C-c") 'sway-mode-fmt)
-    (define-key keymap (kbd "C-c C-a") 'sway-mode-fmt-custom)
-    (define-key keymap (kbd "C-c C-t") 'sway-mode-test)
-    (define-key keymap (kbd "C-c C-b") 'sway-mode-build)
-    (define-key keymap (kbd "C-c C-d") 'sway-mode-deploy)
+    (define-key keymap (kbd "C-c C-c") 'sway-lang-mode-fmt)
+    (define-key keymap (kbd "C-c C-a") 'sway-lang-mode-fmt-custom)
+    (define-key keymap (kbd "C-c C-t") 'sway-lang-mode-test)
+    (define-key keymap (kbd "C-c C-b") 'sway-lang-mode-build)
+    (define-key keymap (kbd "C-c C-d") 'sway-lang-mode-deploy)
     keymap)
-  "Keymap for `sway-mode'.")
+  "Keymap for `sway-lang-mode'.")
 
 
 ;;;###autoload
-(define-derived-mode sway-mode rust-mode
+(define-derived-mode sway-lang-mode rust-mode
   "Sway"
-  (font-lock-add-keywords nil sway-highlights)
-  (use-local-map sway-mode-map))
+  (font-lock-add-keywords nil sway-lang-highlights)
+  (use-local-map sway-lang-mode-map))
 
 
 ;;;###autoload
 (when (featurep 'lsp-mode)
   (add-to-list 'lsp-language-id-configuration
-               '(sway-mode . "sway"))
+               '(sway-lang-mode . "sway"))
   (lsp-dependency 'forc-lsp
                   '(:system "forc-lsp"))
   (lsp-register-client
-   (make-lsp-client :major-modes '(sway-mode)
+   (make-lsp-client :major-modes '(sway-lang-mode)
                     :server-id 'forc-lsp
                     :new-connection (lsp-stdio-connection
                                      (lambda () `(,(lsp-package-path 'forc-lsp)))))))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.sw\\'" . sway-mode))
+(add-to-list 'auto-mode-alist '("\\.sw\\'" . sway-lang-mode))
 
 ;; add to feature list
-(provide 'sway-mode)
-;;; sway-mode.el ends here
+(provide 'sway-lang-mode)
+;;; sway-lang-mode.el ends here
